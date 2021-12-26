194eb8ffe093e1469dd3796f7fce7dae15640c8d
Alejandro Scribano <scribanoalejandro@gmail.com>
2021-12-26 18:04:02 -0300

    templates creados

--- ProyectoIndividual2/AppGestionClinica/urls.py
+++ ProyectoIndividual2/AppGestionClinica/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from AppGestionClinica import views
 
 urlpatterns = [
-    path('',views.inicio),
+    path('',views.inicio, name='Inicio'),
+    path('pacientes/', views.pacientes, name="Pacientes"),
+    path('doctores/', views.doctores, name="Doctores"),
+    path('consultas/', views.consultas, name="Consultas"),
 ]
--- ProyectoIndividual2/AppGestionClinica/views.py
+++ ProyectoIndividual2/AppGestionClinica/views.py
@@ -4,4 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def inicio(request):
-    return render(request,"AppGestionClinica/padre.html")
+    return render(request,"AppGestionClinica/inicio.html")
+
+def pacientes(request):
+    return render(request, "AppGestionClinica/pacientes.html")
+
+def doctores(request):
+    return render(request, "AppGestionClinica/doctores.html")
+
+def consultas(request):
+    return render(request, "AppGestionClinica/consultas.html")
